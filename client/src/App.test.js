780828711631cfad6f7c0b264b4b76ee7e413b7d
weeznog <drew.m.peterson@gmail.com>
2018-08-24 15:59:24 -0700

    intial

---
+++ client/src/App.test.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// tests will use some faker module to mock express response
+it("renders without crashing", () => {});
